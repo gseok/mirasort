22edd7cb2603ca8710752973cc4befa51759a6f2
gyeongseok.seo <gyeongseok.seo@samsung.com>
2015-09-15 18:13:47 +0900

    add default sort logic

--- popup.js
+++ popup.js
@@ -1,7 +1,63 @@
 
-function sortAscend(text) {
-    var result;
+function _ascend(a, b) {
+    console.log('sort ascend');
+    var i;
+    var length;
+    var aChar;
+    var bChar;
+    var aCode;
+    var bCode;
+    var result = 0;
 
+    a = a.trim().toLowerCase();
+    b = b.trim().toLowerCase();
+
+    // get short length
+    length = (a.length >= b.length) ? b.length : a.length;
+
+    // exceptional case
+    // dojo/text!
+    // xstyle/css!
+    if (a.includes('dojo/text') || a.includes('xstyle/css!')) {
+        if (!b.includes('dojo/text!') || !b.includes('xstyle/css!')) {
+            return 1;
+        }
+    }
+
+    // check
+    for (i = 0; i < length; i++) {
+        aChar = a.charAt(i);
+        bChar = b.charAt(i);
+        aCode = aChar.charCodeAt();
+        bCode = bChar.charCodeAt();
+
+        // A: 65, Z:90, a:97, z:122
+        if (aCode === bCode) {
+            continue;
+        } else if (aCode < bCode) {
+            result = -1;
+            break;
+        } else if (aCode > bCode) {
+            result = 1;
+            break;
+        }
+    }
+    return result;
+}
+
+function sortAscend(text) {
+    var texts;
+    var result = '';
+
+    if (text) {
+        texts = text.split('\n');
+        texts.sort(_ascend);
+    }
+
+    // reassemble \n
+    while(texts && texts.length > 0) {
+        result += texts.shift() + '\n';
+    }
 
     return result;
 }
@@ -19,7 +75,8 @@ function onClickSortButton() {
         resultText = sortAscend(srcText);
     }
 
-    return resultText || '';
+    // set text
+    $('#textResult').val(resultText || '');
 }
 
 
